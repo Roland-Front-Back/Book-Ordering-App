fc3194d616f6f2775112a52a8cafe011fb2ffd01
Roland Amoguis <rolandamoguis21@gmail.com>
2025-03-14 15:00:09 +0800

    Aside

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const searchBar = document.getElementById("search-bar");
+const categoryFiltering = document.getElementById("category-filtering");
+const cartButton = document.getElementById("cart-btn");
